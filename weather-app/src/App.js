d123288d064238919e2f122d1d9ec3e52e35c4f9
uperezac <104180226+uperezac@users.noreply.github.com>
2023-01-31 18:38:40 -0500

    Add files via upload

---
+++ weather-app/src/App.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import { getByDisplayValue } from '@testing-library/react'
+
+function App() {
+  const [data,setData] = useState({})
+  const [location, setLocation] = useState('')
+
+const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=574970b33cf378da4cba92eb5d9eb823`
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter') {
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+
+}
+  return (
+    <div className='app'>
+      <div className='search'>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder="How's the weather in ..."
+          type="text" />
+      </div>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>{data.name}</p>
+          </div>
+          <div className='temp'>
+            {data.main ? <h1>{data.main.temp}°F</h1> : null}
+          </div>
+          <div className='description'>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+{data.name !=undefined &&
+        <div className='bottom'>
+        <div className='feels'>
+          {data.main ? <p>{data.main.feels_like}°F</p> : null}
+          <p>Feels Like</p>
+        </div>
+        <div className='humidity'>
+          {data.main ? <p>{data.main.humidity}%</p> : null}
+          <p>Humidity</p>
+        </div>
+        <div className='wind'>
+          {data.wind ? <p>{data.wind.speed} MPH</p> : null}
+          <p>Wind Speed</p>
+        </div>
+      </div>
+}
+      </div>
+    </div>
+  );
+}
+
+export default App;
